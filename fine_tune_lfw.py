bf2cec1affe7af6a8fbf4ce4c5589f18d3e14b34
Xing Di (底兴) <DetionDX@users.noreply.github.com>
2017-05-04 18:38:15 -0400

    Update fine_tune_lfw.py

--- fine_tune_lfw.py
+++ fine_tune_lfw.py
@@ -1,11 +1,7 @@
 ########################################################################################
-# Davi Frossard, 2016                                                                  #
-# VGG16 implementation in TensorFlow                                                   #
-# Details:                                                                             #
-# http://www.cs.toronto.edu/~frossard/post/vgg16/                                      #
-#                                                                                      #
-# Model from https://gist.github.com/ksimonyan/211839e770f7b538e2d8#file-readme-md     #
-# Weights from Caffe converted using https://github.com/ethereon/caffe-tensorflow      #
+# Xing Di, 2017                                                                        #
+# Fituning the VGG16 pretrained weights based on siamese network.                      #
+# This code is inspired by Youngwook Paul Kwon, https://github.com/ywpkwon             #                                                                       #
 ########################################################################################
 
 from __future__ import absolute_import
